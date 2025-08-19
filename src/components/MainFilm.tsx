dc725bfc145144b5ed413053ee70e2c3e933fd42
hanaru <133945249+1roo@users.noreply.github.com>
2025-08-19 10:49:16 +0900

    사진 수정

--- src/components/MainFilm.tsx
+++ src/components/MainFilm.tsx
@@ -2,7 +2,7 @@ const MainFilm = () => {
   return (
     <>
       <img
-        src="/assets/images/film.png"
+        src="/assets/images/film2.png"
         alt="웨딩필름이미지"
         className="mx-auto max-w-[340px]"
       ></img>
